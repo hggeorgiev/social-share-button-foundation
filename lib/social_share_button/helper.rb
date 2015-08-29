85757d8c272c5ae9260b64b877e32386ef949080
Hristo Georgiev <hgeorgievkzr@gmail.com>
2015-08-29 14:45:19 +0300

    Update helper.rb

--- lib/social_share_button/helper.rb
+++ lib/social_share_button/helper.rb
@@ -13,7 +13,7 @@ module SocialShareButton
         special_data = opts.select { |k, _| k.to_s.start_with?('data-' + name) }
 
         link_title = t "social_share_button.share_to", :name => t("social_share_button.#{name.downcase}")
-        html << link_to("","#", {:rel => ["nofollow", rel],
+        html << link_to(raw("<i> class='hi-icon fi-social-#{name} size-40' </i>"),"#", {:rel => ["nofollow", rel],
                                   "data-site" => name,
                                   :class => "social-share-button-#{name}",
                                   :onclick => "return SocialShareButton.share(this);",
