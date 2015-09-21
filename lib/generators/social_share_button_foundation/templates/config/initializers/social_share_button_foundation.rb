65f3d2bb70b4456666615232b6b50e3ba52e0af1
Hristo Georgiev <hgeorgievkzr@gmail.com>
2015-09-21 23:58:41 +0300

    adding google bookmark

--- lib/generators/social_share_button_foundation/templates/config/initializers/social_share_button_foundation.rb
+++ lib/generators/social_share_button_foundation/templates/config/initializers/social_share_button_foundation.rb
@@ -1,3 +1,3 @@
 SocialShareButtonFoundation.configure do |config|
-  config.allow_sites = %w(twitter facebook google-plus  google_bookmark delicious  tumblr pinterest)
+  config.allow_sites = %w(twitter facebook google-plus google-bookmark delicious tumblr pinterest)
 end
